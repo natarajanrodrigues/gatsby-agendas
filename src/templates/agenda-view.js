6519e3c500a84985fb8d01caca112781a530a5af
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2020-02-28 14:59:43 -0300

    I got it!

--- src/templates/agenda-view.js
+++ src/templates/agenda-view.js
@@ -1,31 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { 
-  PDFReader
-  // , MobilePDFReader 
-} from 'reactjs-pdf-reader';
 
 
 
 const AgendaView = ({ data, pageContext }) => {
   
-  let pdfAgenda = data.allFile.edges[0].node.publicURL;
+  // let pdfAgenda = data.allFile.edges[0].node.publicURL;
+  // let pdfAgenda = '/agendas/sousa-2009-01.pdf';
   // pdfAgenda = pdfAgenda.split('false')[0];
-  // const pdfAgenda = `/pdf/${data.allFile.edges[0].node.name}.pdf`;
-  // const pdfAgenda = '/static/sousa-2007-07-c73981cf06c64e9f679f8f3ae3bfbc77.pdf'
-  console.log("TCL: AgendaView -> pdfAgenda", pdfAgenda)
+  const pdfAgenda = `/agendas/${data.allFile.edges[0].node.name}.pdf`;
   
 
   return (
     <>
-      {/* <div style={{overflow:'scroll', width: '80vw', height:'90vh', border: "1px solid black"}}>
-        <embed src={pdfAgenda}  width="100%" height="100%"></embed>  
-      </div>   */}
-
-      <div style={{overflow:'scroll', width: '80vw', height:'100vh', border: "1px solid black"}}>
-        <PDFReader showAllPage scale='1.6' url={pdfAgenda} />  
-        {/* <MobilePDFReader scale="1" isShowHeader={false} showAllPage url={pdfAgenda} />   */}
-      </div>
+      <div style={{overflow:'scroll', width: '80vw', height:'90vh', border: "1px solid black"}}>
+        <embed src={`${pdfAgenda}#toolbar=0`}  width="100%" height="100%"></embed>  
+      </div>  
     </>
   )
 }
@@ -45,7 +35,6 @@ export const query = graphql`
       }
     }
   }
-
 `
 
 export default AgendaView
